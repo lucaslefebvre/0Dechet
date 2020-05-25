b304b968bbf11d34002e9590a5f87c9fbee7712c
PaulBrousses <paul.brousses@gmail.com>
2020-05-25 12:12:06 +0200

    Method to display Category, SubCategory and Types

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -1,24 +1,49 @@
 <?php
-
 namespace App\Controller;
 
-
 use App\Entity\Category;
-use App\Repository\RecipeRepository;
+use App\Entity\SubCategory;
+use App\Entity\Type;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route("/recette", name="recipe_")
+ */
 class RecipeController extends AbstractController
 {
     /**
-     * @Route("/recipe/{slug}", name="recipe")
-     */
+      * @Route("/categorie/{slug}", name="browseByCategory")
+      */
     public function browseByCategory(Category $category)
-
     {
-
         return $this->render('recipe/category.html.twig', [
             'category' => $category,
+            'title' => 'Affichage des recettes selon les catégories'
         ]);
     }
+
+    /**
+     * @Route("/sous-categorie/{slug}", name="browseBySubCategory")
+     */
+    public function browseBySubCategory(SubCategory $subCategory)
+    {
+        return $this->render('recipe/subCategory.html.twig', [
+            'subCategory' => $subCategory,
+            'title' => 'Affichage des recettes selon les sous-catégories'
+        ]);
+    }
+
+    /**
+     * @Route("/type/{slug}", name="browseByType")
+     */
+    public function browseByType(Type $type)
+    {
+        return $this->render('recipe/type.html.twig', [
+            'type' => $type,
+            'title' => 'Affichage des recettes selon les types'
+        ]);
+    }
+
 }
