b46e59316b2ab0039850da57b5dbd43aedb39746
user-lucas <lucas.lefebvre.formation310@gmail.com>
2020-05-25 14:55:21 +0200

    browse method done

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Recipe;
+use App\Form\RecipeType;
+use App\Repository\RecipeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+* @Route("/recette", name="recipe_")
+*/
+class RecipeController extends AbstractController
+{
+    /**
+     * @Route("/", name="browse", methods={"GET"})
+     */
+    public function browse(RecipeRepository $recipeRepository): Response
+    {
+        return $this->render('recipe/browse.html.twig', [
+            'recipes' => $recipeRepository->findAll(),
+            'title' => 'Toutes les recettes'
+        ]);
+    }
+
+    /**
+     * @Route("/ajout", name="new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $recipe->setIngredient(['Pomme']);
+            $recipe->setEquipement(['Balance']);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($recipe);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('recipe_browse');
+        }
+
+        return $this->render('recipe/new.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form->createView(),
+        ]);
+    }
+
+}
