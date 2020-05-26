3726f1f80319ac616b3b76b7d21379082560786f
ManonF38 <manon.furrer@live.fr>
2020-05-26 17:16:31 +0200

    Comment form in recipe show page

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -3,11 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Comment;
 use App\Entity\SubCategory;
 use App\Entity\Recipe;
 use App\Entity\Type;
+use App\Entity\User;
+use App\Form\CommentType;
 use App\Form\RecipeType;
 use App\Repository\RecipeRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -32,13 +37,38 @@ class RecipeController extends AbstractController
   
     /**
      *  Method to display all information about a recipe in template/recipe/show.html.twig
-     * @Route("/{slug}", name="show", methods={"GET"})
+     * @Route("/{slug}", name="show", methods={"GET", "POST"})
      */
-    public function show(Recipe $recipe): Response
+    public function show(Recipe $recipe, Request $request, EntityManagerInterface $em, UserRepository $user): Response
     {
+        // Comment Form
+        $comment = new Comment();
+        $commentForm = $this->createForm(CommentType::class, $comment);
+        $commentForm->handleRequest($request);
+
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+            // Recipe linked to the comment
+            $comment->setRecipe($recipe);
+
+            $comment->setStatus(1);
+            $comment->setCreatedAt(new \DateTime());
+
+            // TODO Modify ths part in order to set the user who is connected
+            $comment->setUser($user->find(1));
+
+            $em = $this->getDoctrine()->getManager();
+            // Cette fois on persiste le genre car c'est un nouvel objet
+            $em->persist($comment);
+            $em->flush();
+
+            return $this->redirectToRoute('recipe_show', [
+                'slug' => $recipe->getSlug(),
+            ]);        }
+
         return $this->render('recipe/show.html.twig', [
             'recipe' => $recipe,
-            'title' => $recipe->getName()
+            'title' => $recipe->getName(),
+            'commentForm' => $commentForm->createView(),
         ]);
     }
 
