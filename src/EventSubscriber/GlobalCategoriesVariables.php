eae605cde32189db722c333f6b112e1ff2f0d380
StevenRenaux <steven.renaux@hotmail.fr>
2020-05-26 18:05:36 +0200

    global for nav done

---
+++ src/EventSubscriber/GlobalCategoriesVariables.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Twig\Environment;
+
+class GlobalCategoriesVariables implements EventSubscriberInterface {
+
+    /**
+     * @var \Twig\Environment
+     */
+    private $twig;
+    /**
+     * @var \Doctrine\ORM\EntityManagerInterface
+     */
+    private $manager;
+
+    public function __construct( Environment $twig, EntityManagerInterface $manager ) {
+        $this->twig    = $twig;
+        $this->manager = $manager;
+    }
+
+    public function injectGlobalVariables() {
+        $categoryGlobal = $this->manager->getRepository(Category::class)->findAll();
+        $this->twig->addGlobal( 'allCategory', $categoryGlobal);
+
+    }
+
+    public static function getSubscribedEvents() {
+        return [ KernelEvents::CONTROLLER =>  'injectGlobalVariables' ];
+    }
+}
