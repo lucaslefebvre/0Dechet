d1b9dc8768a3332c5068c2c12228b2626ab74c3c
PaulBrousses <paul.brousses@gmail.com>
2020-05-29 15:00:52 +0200

    addCommentsOrderBy

--- src/Repository/RecipeRepository.php
+++ src/Repository/RecipeRepository.php
@@ -63,15 +63,4 @@ class RecipeRepository extends ServiceEntityRepository
             ->getResult()
         ;
     }
-    /*public function findCommentsByOrder()
-    {
-        $qb =  $this->createQueryBuilder('r');
-        $qb
-            ->addSelect('c')
-            ->leftJoin('r.comment', 'c')
-            ->orderBy('createdAt', 'DESC')
-        ;
-            return $qb->getQuery()->getResult();
-    }
-*/
 }
