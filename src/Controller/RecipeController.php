44167cec7af268c1d44dd4c3ae84546b109c65cf
user-lucas <lucas.lefebvre.formation310@gmail.com>
2020-06-04 14:15:24 +0200

    done without ajax

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -137,7 +137,7 @@ class RecipeController extends AbstractController
         ]);
     }
 
-        /**
+    /**
      * Method to edit an existing recipe. Send a form, receive the response and flush to the Database
      * @IsGranted("IS_AUTHENTICATED_FULLY")
      * @Route("/edition/{slug}", name="edit", methods={"GET","POST"})
@@ -149,7 +149,6 @@ class RecipeController extends AbstractController
         $image = $recipe->getImage();
         
         $form = $this->createForm(RecipeType::class, $recipe);
-        
         $form->handleRequest($request);
 
 
@@ -187,7 +186,7 @@ class RecipeController extends AbstractController
 
             return $this->redirectToRoute('recipe_show', [
                 'slug' => $recipe->getSlug(),
-                ]);
+            ]);
         }
 
         $formDelete = $this->createForm(DeleteType::class, null, [
