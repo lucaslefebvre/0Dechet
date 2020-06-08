d7ae6f9f923c2381d4e3146b52b988e7c3a27248
user-lucas <lucas.lefebvre.formation310@gmail.com>
2020-06-08 10:03:13 +0200

    captcha debug

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
-    Captcha\Bundle\CaptchaBundle\CaptchaBundle::class => ['all' => true],
 ];
