7e54133f714ca312d19fef23ae3fe87dce43880a
ManonF38 <manon.furrer@live.fr>
2020-06-14 12:21:30 +0200

    bug found

--- src/Services/Slugger.php
+++ src/Services/Slugger.php
@@ -11,7 +11,6 @@ class Slugger
     public function slugify(String $stringToConvert, String $optionalString = null)
     {
         if ($optionalString == null) {
-            
             $search = explode(",",
             "@,ç,æ,œ,á,é,í,ó,ú,à,è,ì,ò,ù,ä,ë,ï,ö,ü,ÿ,â,ê,î,ô,û,å,ø,Ø,Å,Á,À,Â,Ä,È,É,Ê,Ë,Í,Î,Ï,Ì,Ò,Ó,Ô,Ö,Ú,Ù,Û,Ü,Ÿ,Ç,Æ,Œ");
             $replace = explode(",",
@@ -21,7 +20,6 @@ class Slugger
             return preg_replace( '/\W+/', '-', trim(strip_tags($str)));
         }
         else {
-
             $newStringToConvert = $stringToConvert . ' ' . $optionalString;
             $search = explode(",",
             "@,ç,æ,œ,á,é,í,ó,ú,à,è,ì,ò,ù,ä,ë,ï,ö,ü,ÿ,â,ê,î,ô,û,å,ø,Ø,Å,Á,À,Â,Ä,È,É,Ê,Ë,Í,Î,Ï,Ì,Ò,Ó,Ô,Ö,Ú,Ù,Û,Ü,Ÿ,Ç,Æ,Œ");
